1541af9d19a7464cb8385987d61d573edcc69a50
Arvydas <arvydas.simbelis@gmail.com>
2023-11-21 01:17:02 +0200

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,78 @@
+import { IoLanguageSharp } from 'react-icons/io5';
+import { IoIosArrowForward } from "react-icons/io";
+import { RxTriangleDown } from "react-icons/rx";
+import logo from './img/logo.png';
+import style from './Header.module.css';
+import { useState } from 'react';
+
+
+export function Header(){
+    const [EmailPlaceholder, setEmailPlaceholder] = useState('Email address');
+    const [subscribeLabelHide, setSubscribeLabelHide] = useState('none');
+    const [emailInputClassName, setEmailInputClassName ] = useState(style.subscribeInputIdle);
+    const [emailInputValue, setEmailInputValue] = useState('');
+    
+    function emailGotFocus(){
+        setEmailPlaceholder('');
+        setSubscribeLabelHide('flex')
+        setEmailInputClassName(style.subscribeInputActive);
+    }
+    function emaillLostFocus(){
+        if (emailInputValue === ''){
+            setEmailPlaceholder('Email address');
+            setSubscribeLabelHide('none')
+            setEmailInputClassName(style.subscribeInputIdle);
+        }
+        
+    }
+
+
+    const emailInput = <input className={emailInputClassName} 
+                              type='text' 
+                              placeholder={EmailPlaceholder} 
+                              onFocus={emailGotFocus} 
+                              onBlur={emaillLostFocus}
+                              onChange={e => setEmailInputValue(e.target.value)}
+                              value={emailInputValue}>
+                       </input>;
+
+    return(
+        <div className={style.header}>
+            <div className={style.headerContent}>
+                <div className={style.menuTop}>
+                    <div className={style.logoContainer}>
+                        <img className={style.logo} src={logo} alt='bitflix logo'/>
+                    </div>
+                    <div className={style.menu}>
+                        <div className={style.comboBox}>
+                            <div><IoLanguageSharp/></div>
+                            <div>English</div>
+                            <div><RxTriangleDown /></div>
+                        </div>
+                        <div className={style.loginBtn}>Sign In</div>    
+                    </div>
+                    
+                </div>
+
+                <div className={style.headerMain}>
+                    <div className={style.title}>
+                        Unlimited movies, TV shows, and more
+                    </div>
+                    <div className={style.subtitle}>Watch anywhere. Cancel anytime</div>
+                    <div className={style.subscribeIntro}>
+                        Ready to watch? Enter your email to createe or restart your membership.
+                    </div>
+                    <div className={style.subscribeContainer}>
+                        <div className={style.inputbox}>
+                            <div className={style.subscribeLabel} style={{display: subscribeLabelHide}}>Email address</div>
+                            {emailInput}     
+                        </div>
+                        <div className={style.subscribeBtn}>
+                            Get Started <IoIosArrowForward />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
