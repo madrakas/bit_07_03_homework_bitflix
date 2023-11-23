1de473ea0f021deb6957049d7c21bfc897d2cc93
Arvydas <arvydas.simbelis@gmail.com>
2023-11-24 00:22:37 +0200

    Ad section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { AdSection } from './components/ads/AdSection';
 import { Header } from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <AdSection />
     </div>
   );
 }
